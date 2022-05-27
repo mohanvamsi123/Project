df05b17f32bf85a823e51cd3705b71d56b711cde
mohanvamsi123 <mohhanvamsi.d1998@gmail.com>
2022-05-27 17:02:15 +0530

    Revert "Update environment.ts"
    This reverts commit 875f0bedd5fa2206d7aa548915da2d3b120b8d75.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  api_url: 'https://spring850.herokuapp.com'
+  production: false,
+  api_url: 'https://spring-stg.herokuapp.com'
 };
 
 /*
